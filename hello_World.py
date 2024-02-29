a7b466f7142f8f9fac0ba07f7493ff28b05f753a
Diego Saavedra <dmsaavedra@espe.edu.ec>
2024-02-28 21:31:26 -0500

    Añadir archivo hello_world.py simbolo de admiracion

--- hello_World.py
+++ hello_World.py
@@ -1,6 +1,6 @@
 def welcome_message():
   name = input("Ingrese su nombre: ")
-  print("Bienvenio", name, "al curso de Django y React")
+  print("Bienvenio", name, "al curso de Django y React!")
 
 if __name__ == "__main__":
   welcome_message()
